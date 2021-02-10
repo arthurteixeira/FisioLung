b0bbabaa2fd77612c62c9bb2546b2870314094c9
arthurteixeira <arthurtjardim@gmail.com>
2021-02-10 10:34:44 -0300

    3 eixos funcionando com fft

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,5 +1,19 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include "arduinoFFT.h"
+
+#define SAMPLES 64             //Must be a power of 2
+#define SAMPLING_FREQUENCY 200 //Hz, must be less than 10000 due to ADC
+
+arduinoFFT FFT = arduinoFFT();
+ 
+unsigned int sampling_period_us;
+unsigned long microseconds;
+ 
+double vReal[SAMPLES];
+double vRealY[SAMPLES];
+double vRealZ[SAMPLES];
+double vImag[SAMPLES];
 
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
@@ -8,14 +22,9 @@
 MPU6050 accelgyro;
 
 int16_t ax, ay, az;
-int16_t gx, gy, gz;
+int gx, gy, gz;
 
 #define OUTPUT_READABLE_ACCELGYRO
-
-char string[50];
-char sax[10];
-char say[10];
-char saz[10];
 
 #define LED_PIN 13
 bool blinkState = false;
@@ -31,30 +40,60 @@ void setup() {
     Serial.begin(38400);
 
     accelgyro.initialize();
-
+    sampling_period_us = round(1000000*(1.0/SAMPLING_FREQUENCY));
+    
     // configure Arduino LED for
     pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-    // read raw accel/gyro measurements from device
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    for(int i=0; i<SAMPLES; i++){
+        microseconds = micros();    //Overflows after around 70 minutes!
 
-    #ifdef OUTPUT_READABLE_ACCELGYRO
+        accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+        vReal[i] = (ax/16384)*9.81;
+        vRealY[i] = (ay/16384)*9.81;
+        vRealZ[i] =  (az/16384)*9.81;
+        vImag[i] = 0;
+     
+        while(micros() < (microseconds + sampling_period_us)){
+        }
+    }
 
-        Serial.print("x"); Serial.println(ax);
-        Serial.print("y"); Serial.println(ay);
-        Serial.print("z"); Serial.println(az);
+    FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+    FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+    double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
+    
+    Serial.print("xp"); Serial.println(peak);
+
+    FFT.Windowing(vRealY, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.Compute(vRealY, vImag, SAMPLES, FFT_FORWARD);
+    FFT.ComplexToMagnitude(vRealY, vImag, SAMPLES);
+    double peakY = FFT.MajorPeak(vRealY, SAMPLES, SAMPLING_FREQUENCY);
+
+    Serial.print("yp"); Serial.println(peakY);
+
+    FFT.Windowing(vRealZ, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.Compute(vRealZ, vImag, SAMPLES, FFT_FORWARD);
+    FFT.ComplexToMagnitude(vRealZ, vImag, SAMPLES);
+    double peakZ = FFT.MajorPeak(vRealZ, SAMPLES, SAMPLING_FREQUENCY);
+
+    Serial.print("zp"); Serial.println(peakZ);
+    
+    for(int i=0; i<(SAMPLES/2); i++){
+        /*View all these three lines in serial terminal to see which frequencies has which amplitudes*/
+         
+        Serial.print("ff"); Serial.println((i * 1.0 * SAMPLING_FREQUENCY) / SAMPLES, 1);
+        Serial.print("xa"); 
+        Serial.println(vReal[i]);    //View only this line in serial plotter to visualize the bins
+        Serial.print("ya"); 
+        Serial.println(vRealY[i]);
+        Serial.print("za"); 
+        Serial.println(vRealZ[i]);
         
-    #endif
-
-    #ifdef OUTPUT_BINARY_ACCELGYRO
-        Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
-        Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
-        Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
-    #endif
-
-
-     delay(100);
+    }
+    Serial.println("-");
+    delay(100);
     
 }
